8c9892ea89d8e062846263e01c123c824191115b
vikashkumars <vikashkumars@gmail.com>
2025-09-17 11:10:20 -0700

    Laying the foundation

--- App.js
+++ App.js
@@ -1,58 +1,58 @@
-
-/**
- * 
-
-const heading = React.createElement(
-    "h1",{id:"heading",xyz:"abc"},"Hello World"
-);
-console.log(heading);//Object
-const root = ReactDOM.createRoot(
-    document.getElementById("root")
-);
- */
-/** 
- *  <div id= "parent">
- *      <div id="child">
- *          <h1>I am H1 tag</h1>
- *          <h2>I am H2 tag</h2>
- *      </div>
- * <div id="child 2">
- *          <h1>I am H1 tag</h1>
- *          <h2>I am H2 tag</h2>
- *      </div>
- *  </div> 
- * 
- * 
- * 
-*/
 import React from "react";
 import ReactDOM from "react-dom/client";
+const jsxheading = (
+  <h1 className="head" tabIndex="5">
+    
+    Hi Vikash Here
+  </h1>
+);
+const elem=(
+    <span>React elemet</span>
+);
+const title = (
+    <h1 className="head" tabIndex="5">
+        {elem}
+        Title example
+    </h1>
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  [
-    React.createElement(
-      "div",
-      { id: "child", key: "child1-container" },
-      [
-        React.createElement("h1", { id: "child1", key: "h1-1" }, "I am h1 tag"),
-        React.createElement("h2", { id: "child2", key: "h2-1" }, "I am h2 tag  ")
-      ]
-    ),
-    React.createElement(
-      "div",
-      { id: "Testo", key: "child2-container" },
-      [
-        React.createElement("h1", { id: "child3", key: "h1-2" }, "I am h1 tag"),
-        React.createElement("h2", { id: "child4", key: "h2-2" }, "I am h2 tag")
-      ]
-    )
-  ]
+);
+/**
+ * const elem=(
+    <span>React elemet</span>
+);
+// how to call react elemet inside another react element
+ * const Title = () => (
+    <h1 className="head" tabIndex="5">
+    {elem // calling react element inside another react element}
+    Title example
+    </h1>
+
+);
+ * @returns Calling function inside another function is 
+        called component composition
+const HeadingComponent = () => (
+    <div id="container">
+      <h2>{num}</h2>// can call any java script code inside {} 
+      // How to call react element inside another react component
+        <Title /> //component composition<Title></Title>// call a function {Title()}//  wrong way to call a function
+        {title} // calling react element inside another react component
+       <h1>This is a heading component</h1>;
+    </div>
+    
+);
+ */
+const num = 1000
+const HeadingComponent = () => (
+    <div id="container">
+          {title} 
+       <h1>This is a heading component</h1>
+    </div>
+    
 );
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+//root.render(title);// react element calling
+root.render(<HeadingComponent />);// react component calling
     
  
